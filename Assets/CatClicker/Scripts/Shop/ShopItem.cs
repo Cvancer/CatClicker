904ec0120509e35abc2c8e212cbc3edac930e206
Cvancer <kate974club@gmail.com>
2025-06-06 18:15:39 +0300

    change room add sounds

--- Assets/CatClicker/Scripts/Shop/ShopItem.cs
+++ Assets/CatClicker/Scripts/Shop/ShopItem.cs
@@ -6,15 +6,15 @@ public class ShopItem : ScriptableObject
     public ShopItemType Type => _type;
     public Sprite Image => _image;
     public int Price => _price;
-    public string Name => _name;
     public int Amount => _amount;
     public int ID => _id;
+    public string Kind => _kind; 
 
 
     [SerializeField] private ShopItemType _type;
     [SerializeField] private Sprite _image;
     [SerializeField] private int _price;
-    [SerializeField] private string _name;
     [SerializeField] private int _amount;
     [SerializeField] private int _id;
+    [SerializeField] private string _kind;
 }
--- Assets/CatClicker/Scripts/UI/UIRoot.cs
+++ Assets/CatClicker/Scripts/UI/UIRoot.cs
@@ -1,3 +1,4 @@
+using UnityEditor.ShaderGraph.Drawing;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,19 +7,42 @@ public class UIRoot : MonoBehaviour
     [SerializeField] private ClickButton _clickButton;
     [SerializeField] private FishText _fishText;
     [SerializeField] private Shop _shop;
-    [SerializeField] private Slider _slider;
-    private GameState _gameState;
+    [SerializeField] private Menu _menu;
+    [SerializeField] private GameObject _lightOff;
     public void Initialize(GameState gameState)
     {
-        _gameState = gameState;
-        _shop.Initialize(gameState);
+        _shop.Initialize(gameState, this);
         _clickButton.Initialize(gameState);
         _fishText.Initialize(gameState.FishStorage);
-        _slider.SetValueWithoutNotify(_gameState.Volume);
+        _menu.Initialize(gameState);
+        
+    }
+    public void DeactivateAll()
+    {
+        _shop.SetActive(false);
+        _fishText.gameObject.SetActive(false);
+        _menu.gameObject.SetActive(false);
+        _clickButton.gameObject.SetActive(false);
+
+    }
+    public void ActivateAll()
+    {
+        _shop.SetActive(true);
+        _fishText.gameObject.SetActive(true);
+        _menu.gameObject.SetActive(true);
+        _clickButton.gameObject.SetActive(true);
+
     }
 
-    public void SetVolume(float volume)
+    public void LightOff()
     {
-        _gameState.SetVolume(volume);
+        _lightOff.gameObject.SetActive(true);
     }
+
+    public void LightOn()
+    {
+        _lightOff.gameObject.SetActive(false);
+    }
+
+
 }
